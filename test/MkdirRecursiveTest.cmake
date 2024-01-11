f2510244fb3652245479957db3698e6e70a233f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-11 20:09:50 +0700

    test: add status message when running `MkdirRecursiveTest.cmake`

--- test/MkdirRecursiveTest.cmake
+++ test/MkdirRecursiveTest.cmake
@@ -1,12 +1,16 @@
 if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/parent)
+  message(STATUS "Removing test directory")
   file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/parent)
 endif()
 
 include(MkdirRecursive)
+
+message(STATUS "Creating test directory recursively")
 mkdir_recursive(${CMAKE_CURRENT_BINARY_DIR}/parent/child)
 
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/parent/child)
-  file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/parent)
-else()
+if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/parent/child)
   message(FATAL_ERROR "Directory `${CMAKE_CURRENT_BINARY_DIR}/parent/child` should exist!")
 endif()
+
+message(STATUS "Removing test directory")
+file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/parent)
