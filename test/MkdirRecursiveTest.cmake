1763b03af97b8b4619958527ccabd32a4cd785e6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-12 21:53:57 +0800

    test: modify testing from requires to pass build directory (#25)

--- test/MkdirRecursiveTest.cmake
+++ test/MkdirRecursiveTest.cmake
@@ -4,22 +4,22 @@ if(NOT TEST_MATCHES)
 endif()
 
 if("Create directory recursively" MATCHES ${TEST_MATCHES})
-  if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/parent)
+  if(EXISTS parent)
     message(STATUS "Removing test directory")
-    file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/parent)
+    file(REMOVE_RECURSE parent)
   endif()
 
   include(MkdirRecursive)
 
   message(STATUS "Creating test directory recursively")
-  mkdir_recursive(${CMAKE_CURRENT_BINARY_DIR}/parent/child)
+  mkdir_recursive(parent/child)
 
-  if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/parent/child)
-    message(FATAL_ERROR "Directory `${CMAKE_CURRENT_BINARY_DIR}/parent/child` should exist!")
+  if(NOT EXISTS parent/child)
+    message(FATAL_ERROR "Directory `parent/child` should exist!")
   endif()
 
   message(STATUS "Removing test directory")
-  file(REMOVE_RECURSE ${CMAKE_CURRENT_BINARY_DIR}/parent)
+  file(REMOVE_RECURSE parent)
 endif()
 
 # Add more test cases here.
