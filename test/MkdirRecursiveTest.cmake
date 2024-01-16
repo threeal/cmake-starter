e55fe14b1a2c4f77202542e82fc24b4916cab6c6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-16 19:26:21 +0700

    test: assert if there's nothing to test (#29)
    * test: add assertion if there's nothing to test
    
    * test: fix regex for passing `TEST_MATCHES` option

--- test/MkdirRecursiveTest.cmake
+++ test/MkdirRecursiveTest.cmake
@@ -3,7 +3,11 @@ if(NOT TEST_MATCHES)
   set(TEST_MATCHES ".*")
 endif()
 
+set(TEST_COUNT 0)
+
 if("Create directory recursively" MATCHES ${TEST_MATCHES})
+  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
   if(EXISTS parent)
     message(STATUS "Removing test directory")
     file(REMOVE_RECURSE parent)
@@ -24,5 +28,11 @@ endif()
 
 # Add more test cases here.
 if("Test name" MATCHES ${TEST_MATCHES})
+  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
   # Do something.
+endif()
+
+if(TEST_COUNT LESS_EQUAL 0)
+  message(FATAL_ERROR "Nothing to test with: ${TEST_MATCHES}")
 endif()
