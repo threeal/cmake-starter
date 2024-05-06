db674943a6051a757abfcf3c2a8fbd077cd4bcc6
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-06 21:12:49 +0700

    test: simplify sample `mkdir_recursive` testing

--- test/cmake/MkdirRecursiveTest.cmake
+++ test/cmake/MkdirRecursiveTest.cmake
@@ -1,20 +1,13 @@
+include(MkdirRecursive)
+
 function("Create directory recursively")
-  if(EXISTS parent)
-    message(STATUS "Removing test directory")
-    file(REMOVE_RECURSE parent)
-  endif()
+  file(REMOVE_RECURSE parent)
 
-  include(MkdirRecursive)
-
-  message(STATUS "Creating test directory recursively")
   mkdir_recursive(parent/child)
 
   if(NOT EXISTS parent/child)
-    message(FATAL_ERROR "Directory `parent/child` should exist!")
+    message(FATAL_ERROR "expected path 'parent/child' to exist")
   endif()
-
-  message(STATUS "Removing test directory")
-  file(REMOVE_RECURSE parent)
 endfunction()
 
 # Add more test cases here.
