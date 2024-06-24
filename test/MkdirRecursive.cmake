ba48341f64ddaef3e20f2876b311cd2ff434e6d0
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-06-24 15:20:11 +0700

    test: utilize `Assertion.cmake` module (#81)

--- test/MkdirRecursive.cmake
+++ test/MkdirRecursive.cmake
@@ -1,11 +1,9 @@
 cmake_minimum_required(VERSION 3.5)
 
 find_package(MyMkdir REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
+include(Assertion.cmake)
 
 file(REMOVE_RECURSE parent)
 
 mkdir_recursive(parent/child)
-
-if(NOT EXISTS parent/child)
-  message(FATAL_ERROR "expected path 'parent/child' to exist")
-endif()
+assert(EXISTS parent/child)
