d7ea8322ecf60ded2e17ce8f6f33a9e5c08e0796
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-11 16:14:32 +0700

    test: organize test cases in sections

--- test/mkdir_recursive.cmake
+++ test/mkdir_recursive.cmake
@@ -1,6 +1,10 @@
 find_package(MyMkdir REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
 
-file(REMOVE_RECURSE parent)
+section("it should create a directory recursively")
+  file(REMOVE_RECURSE parent)
 
-mkdir_recursive(parent/child)
-assert(EXISTS parent/child)
+  mkdir_recursive(parent/child)
+
+  assert(EXISTS parent/child)
+  assert(IS_DIRECTORY parent/child)
+endsection()
