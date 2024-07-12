4158a21857662dca96d7b807e5386594fa64d12c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-12 18:13:11 +0700

    test: organize tests (#87)
    * test: rename test files to use lower-case names
    
    * test: use test file as the test name
    
    * test: organize test cases in sections
    
    * ci: run test verbosely

--- test/MkdirRecursive.cmake
+++
@@ -1,6 +0,0 @@
-find_package(MyMkdir REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
-
-file(REMOVE_RECURSE parent)
-
-mkdir_recursive(parent/child)
-assert(EXISTS parent/child)
---
+++ test/mkdir_recursive.cmake
@@ -0,0 +1,10 @@
+find_package(MyMkdir REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
+
+section("it should create a directory recursively")
+  file(REMOVE_RECURSE parent)
+
+  mkdir_recursive(parent/child)
+
+  assert(EXISTS parent/child)
+  assert(IS_DIRECTORY parent/child)
+endsection()
