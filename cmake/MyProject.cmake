947aa40aa6250f5f9b7f60a4dcba2bf6722b1b85
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-02 13:06:46 +0700

    feat: replace sample function to `git_clone` function
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- cmake/MyProject.cmake
+++ cmake/MyProject.cmake
@@ -1,3 +1,15 @@
-function(mkdir_recursive DIR_PATH)
-  file(MAKE_DIRECTORY "${DIR_PATH}")
+# Clone a Git repository to a specified directory.
+#
+# git_clone(<url> <dir>)
+#
+# This function clones a Git repository from the `<url>` and saves it to the
+# `<dir>` directory. It outputs a fatal error message if the operation fails.
+function(git_clone URL DIR)
+  execute_process(
+    COMMAND git clone "${URL}" "${DIR}"
+    ERROR_VARIABLE ERR
+    RESULT_VARIABLE RES)
+  if(NOT RES EQUAL 0)
+    message(FATAL_ERROR "Failed to clone '${URL}': ${ERR}")
+  endif()
 endfunction()
