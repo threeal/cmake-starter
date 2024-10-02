1aac0d6936d8e8204a7c0b0b28f77c8c0008c37c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-02 13:07:51 +0700

    feat: modify sample project (#105)
    * feat: rename sample project to `MyProject`
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: replace sample function to `git_clone` function
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ test/git_clone.cmake
@@ -0,0 +1,20 @@
+find_package(MyProject REQUIRED PATHS ${CMAKE_CURRENT_LIST_DIR}/../cmake)
+
+section("it should clone a Git repository")
+  file(REMOVE_RECURSE repo)
+
+  git_clone(https://github.com/threeal/cmake-starter repo)
+
+  assert(EXISTS repo)
+  assert_execute_process(git -C repo rev-parse --is-inside-work-tree)
+endsection()
+
+section("it should fail to clone an invalid Git repository")
+  file(REMOVE_RECURSE repo)
+
+  assert_fatal_error(
+    CALL git_clone https://github.com repo
+    MESSAGE "Failed to clone 'https://github.com'")
+
+  assert(NOT EXISTS repo)
+endsection()
