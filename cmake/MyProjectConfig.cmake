50f3a667836c7a56c699c5012b2ac2565696f55e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-02 20:59:40 +0700

    docs: add `TODO` comments in files
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- cmake/MyProjectConfig.cmake
+++ cmake/MyProjectConfig.cmake
@@ -1,1 +1,4 @@
+# TODO: Rename this module to match the correct project name.
+
+# TODO: Replace `MyProject.cmake` with the correct main module file.
 include(${CMAKE_CURRENT_LIST_DIR}/MyProject.cmake)
